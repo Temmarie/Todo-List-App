c10f5db9599df346bc324670b4e567d8f614d045
Temmarie <temmar_whyte16@yahoo.com>
2020-11-19 12:09:05 +0100

    Add first test

--- tests/create-todo.test.js
+++ tests/create-todo.test.js
@@ -1,9 +1,29 @@
-const createTodo = require('../src/create-todo');
-
-test('title to be title', () => {
-  expect(createTodo.title).toBe() ;
+const todoList = (title, description, dueDate, priority) => ({
+  title, description, dueDate, priority,
 });
-
-// test('Create todo should have title feature', ()=> {
-//   expect(typeof createTodo().toBe('function'));
-// });
+const todoForm = {
+    title: {
+        value: 'Tife'
+    },
+    description: {
+        value: 'Tife-List'
+    },
+    date: {
+        value: new Date()
+    },
+    priority: {
+        value: 1
+    },
+}
+const createTodo = () => {
+  const title = todoForm.title.value;
+  const description = todoForm.description.value;
+  const dueDate = todoForm.date.value;
+  const priority = todoForm.priority.value;
+  return todoList(title, description, dueDate, priority);
+};
+test('Creating New Todo', () => {
+    const action = createTodo();
+    const expectation = {title: "Tife", description: "Tife-List", dueDate: new Date(), priority: 1};
+    expect(action).toEqual(expectation);
+});
